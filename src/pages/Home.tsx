6fb9e5b4d3f3b3d0cd92b88d26cfbf593512edf4
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-10-16 19:44:38 +0400

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,51 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonCheckbox, IonLabel, IonNote, IonBadge } from '@ionic/react';
 import React from 'react';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </IonPage>
-  );
-};
+class Home extends React.Component {
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Ionic Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonList>
+            <IonItem>
+              <IonCheckbox slot="start"/>
+              <IonLabel>
+                <h2>Create Idea</h2>
+                <IonNote>Run Idea by Brandy</IonNote>
+              </IonLabel>
+              <IonBadge color="success" slot="end">
+                5 Days
+              </IonBadge>
+            </IonItem>
+            <IonItem>
+              <IonCheckbox slot="start"/>
+              <IonLabel>
+                <h2>Create Idea</h2>
+                <IonNote>Run Idea by Brandy</IonNote>
+              </IonLabel>
+              <IonBadge color="success" slot="end">
+                5 Days
+              </IonBadge>
+            </IonItem>
+          </IonList>
+
+          The world is your oyster.
+          <p>
+            If you get lost, the{' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
+              docs
+            </a>{' '}
+            will be your guide.
+          </p>
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
 
 export default Home;
